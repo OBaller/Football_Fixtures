f46334eeadcee5f9f28d06c1b05cff98aa6b3591
brightsummit.co <naseemoyebola@gmail.com>
2021-08-27 16:04:07 +0100

    register cells

---
+++ Football_fixtures/Football_fixtures/TableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewCell.swift
+//  Football_fixtures
+//
+//  Created by IBE on 27/08/2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var timedLabel: UILabel!
+    @IBOutlet weak var matchTimeLabel: UILabel!
+    
+    @IBOutlet weak var gameWeekLabel: UILabel!
+    @IBOutlet weak var fixtureLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var gameMinuteLabel: UILabel!
+    
+}
--- Football_fixtures/Football_fixtures/ViewController.swift
+++ Football_fixtures/Football_fixtures/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var gamesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        gamesTableView.delegate = self
+        gamesTableView.dataSource = self
+        
     }
-
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        return cell
+    }
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//
+//    }
+}
