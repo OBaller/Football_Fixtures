7164bfc0f01c40fa14f2e00a595a44163eda15dd
brightsummit.co <naseemoyebola@gmail.com>
2021-08-29 00:41:30 +0100

    feat: competition tab

---
+++ Football_fixtures/SecondTab/CompetitionTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CompetitionTableViewCell.swift
+//  Football_fixtures
+//
+//  Created by IBE on 29/08/2021.
+//
+
+import UIKit
+
+class CompetitionTableViewCell: UITableViewCell {
+    @IBOutlet weak var competitionLabel: UILabel!
+
+}
--- Football_fixtures/SecondTab/CompetitionViewController.swift
+++ Football_fixtures/SecondTab/CompetitionViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class CompetitionViewController: UIViewController {
-
+    @IBOutlet weak var compTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        compTableView.delegate = self
+        compTableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
-    
 
+}
+
+extension CompetitionViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CompetitionTableViewCell
+        return cell
+    }
+    
+   
 }
